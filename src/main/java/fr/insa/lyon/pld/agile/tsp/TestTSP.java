be6aa0bb3980daf15cb64ca317b9c35fa1ec938e
Alexandre van Beurden <alexandre.vanbeurden78@gmail.com>
2018-11-26 15:19:21 +0100

    Update TSP and change few variables names

--- src/main/java/fr/insa/lyon/pld/agile/tsp/TestTSP.java
+++ src/main/java/fr/insa/lyon/pld/agile/tsp/TestTSP.java
@@ -12,11 +12,7 @@ public class TestTSP {
 	}
     }
     public static boolean testTSP() {
-
 	if(!testHeuristic()) {
-	    return false;
-	}
-	/*if(!testBogusCostDuration()) {
 	    return false;
 	}
 	if(!testSingleValue()) {
@@ -24,7 +20,7 @@ public class TestTSP {
 	}
 	if(!testEmptyValue()) {
 	    return false;
-	}*/
+	}
 	return true;
     }
     public static boolean testHeuristic() {
@@ -51,33 +47,11 @@ public class TestTSP {
 
 	return true;
     }
-
-    public static boolean testBogusCostDuration() {
-	int nb=10;
-
-	int[][] cost = generateBogusCost(nb);
-	int[] durr = generateBogusDurr(nb);
-
-	TSP tsp = new TSP1();
-	tsp.chercheSolution(100000, nb, cost, durr);
-	int val = tsp.getCoutMeilleureSolution();
-
-	TSP tsp1 = new TSP1();
-	tsp1.disableBound();
-	tsp1.chercheSolution(100000, nb, cost, durr);
-	int val1 = tsp1.getCoutMeilleureSolution();
-
-	if(val!=val1) {
-	    return false;
-	}
-
-	return true;
-    }
     public static boolean testSingleValue() {
 	int nb=1;
 
-	int[][] cost = generateBogusCost(nb);
-	int[] durr = generateBogusDurr(nb);
+	int[][] cost = generateRandomCost(nb);
+	int[] durr = generateRandomDurr(nb);
 
 	TSP tsp = new TSP1();
 	tsp.chercheSolution(100000, nb, cost, durr);
@@ -98,8 +72,8 @@ public class TestTSP {
     public static boolean testEmptyValue() {
 	int nb=0;
 
-	int[][] cost = generateBogusCost(nb);
-	int[] durr = generateBogusDurr(nb);
+	int[][] cost = generateRandomCost(nb);
+	int[] durr = generateRandomDurr(nb);
 
 	TSP tsp = new TSP1();
 	tsp.chercheSolution(100000, nb, cost, durr);
@@ -134,45 +108,6 @@ public class TestTSP {
 	Random rand = new Random();
 	for(int i=0;i<nb;++i){
 	    durr[i]=rand.nextInt(1000);
-	}
-	return durr;
-    }
-
-    public static int[][] generateBogusCost(int nb){
-	int[][] cost = new int[nb][nb];
-	Random rand = new Random();
-
-	for(int i=0;i<nb;++i){
-	    for(int j=0;j<nb;++j){
-		int randomInt = rand.nextInt(4);
-		if(randomInt==0) {
-		    cost[i][j]=rand.nextInt(2000)-1000;
-		}
-		else if(randomInt==1) {
-		    cost[i][j]=Integer.MAX_VALUE;
-		}
-		else if(randomInt==2) {
-		    cost[i][j]=Integer.MIN_VALUE;
-		}
-	    }
-	}
-	return cost;
-    }
-
-    public static int[] generateBogusDurr(int nb){
-	int[] durr = new int[nb];
-	Random rand = new Random();
-	for(int i=0;i<nb;++i){
-	    int randomInt = rand.nextInt(4);
-	    if(randomInt==0) {
-		durr[i]=rand.nextInt(2000)-1000;
-	    }
-	    else if(randomInt==1) {
-		durr[i]=Integer.MAX_VALUE;
-	    }
-	    else if(randomInt==2) {
-		durr[i]=Integer.MIN_VALUE;
-	    }
 	}
 	return durr;
     }
