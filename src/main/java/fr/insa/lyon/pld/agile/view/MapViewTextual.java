a905a16bbf8a31f6f5163bdebaea4d94be0213b1
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2018-11-28 02:03:00 +0100

    Changes committed

--- src/main/java/fr/insa/lyon/pld/agile/view/MapViewTextual.java
+++ src/main/java/fr/insa/lyon/pld/agile/view/MapViewTextual.java
@@ -3,11 +3,12 @@ package fr.insa.lyon.pld.agile.view;
 import fr.insa.lyon.pld.agile.model.*;
 
 import java.awt.GridLayout;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 import javax.swing.*;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  *
@@ -15,42 +16,67 @@ import java.util.List;
  */
 public class MapViewTextual extends JTabbedPane implements MapView
 {
-    Map map;
-    List<Delivery> deliveries;
+    Map map = null;
+    List<Delivery> deliveries = null;
     
-    
+    List<JList> lists = null;
     
     public MapViewTextual()
     {
-        // this.addTab();
-        
-        for (int count=0; count<3; count++) {
-            String livreurName = "Livreur " + (count+1);
-            JPanel panLivreur = makeListPanel(livreurName);
-            this.addTab(livreurName, panLivreur); //, null, panLivreur, livreurName);
-        }
-    }
-    
-    protected static JPanel makeListPanel(String text) {
-        JPanel pan = new JPanel();
-        pan.setLayout(new GridLayout(1, 1));
-        
-        String list[] = {"Monday", "Tuesday", "Wednesday",
-                         "Thursday", "Friday", "Saturday", "Sunday"};
-        pan.add(new JList<>(list));
-        return pan;
+        recreate();
     }
     
     @Override
     public void setMap(Map newMap)
     {
+        map = newMap;
+        deliveries = null;
         
+        recreate();
     }
     
     @Override
     public void setDeliveries(List<Delivery> newDeliveries)
     {
+        deliveries = newDeliveries;
         
+        recreate();
+    }
+    
+    protected void recreate() {
+        lists = new ArrayList<>();
+        
+        this.removeAll();
+        
+        Vector<String> locs = new Vector();
+        
+        if (deliveries != null) {
+            for (Delivery d : deliveries) {
+                locs.add("Point " + d.getNode().getId());
+            }
+        }
+        
+        newTab("Tous", locs);
+        
+        
+        for (int count=1; count<=3; count++) {
+            Vector<String> vect = new Vector<>();
+            for (int k=1; k<=count*3; k++) {
+                vect.addElement("Point " + k);
+            }
+            
+            newTab("Livreur " + count, vect);
+        }
+    }
+    
+    protected void newTab(String tabName, Vector<String> tabList) {
+        JList<String> lstList = new JList<>(tabList);
+        lists.add(lstList);
+        
+        JPanel panLivreur = new JPanel();
+        panLivreur.setLayout(new GridLayout(1, 1));
+        panLivreur.add(lstList);
+        this.addTab(tabName, panLivreur); //, null, panLivreur, livreurName);
     }
     
 }
