24d20d19c34343e4e2a92575a2ae6b0d554085ca
Alexandre van Beurden <alexandre.vanbeurden78@gmail.com>
2018-12-10 22:42:40 +0100

    Update indent

--- src/test/java/fr/insa/lyon/pld/agile/KmeansTest.java
+++ src/test/java/fr/insa/lyon/pld/agile/KmeansTest.java
@@ -13,28 +13,27 @@ public class KmeansTest {
 
     @Test
     public void testClustering() {
-	int clusterNb=9;
-	int nodesNb=1000;
-	int width=100;
-	int height=50;
-	List<Node> nodes = generateNodes(nodesNb,width,height);
+        int clusterNb=9;
+        int nodesNb=1000;
+        int width=100;
+        int height=50;
+        List<Node> nodes = generateNodes(nodesNb,width,height);
 
-	int[] clusters = KMeans.kMeans(nodes, clusterNb, null);
+        int[] clusters = KMeans.kMeans(nodes, clusterNb, null);
 
-	for(int i=0; i<clusters.length; ++i) {
-	    assertTrue(clusters[i]>=0 && clusters[i]<clusterNb);
-	}
-
+        for(int i=0; i<clusters.length; ++i) {
+                assertTrue(clusters[i]>=0 && clusters[i]<clusterNb);
+        }
     }
 
     private static List<Node> generateNodes(int nodesNb, int spaceHeight, int spaceWidth) {
-	List<Node> nodes = new ArrayList<>();
-	for(int i=0; i<nodesNb; ++i) {
-	    double lon = ( (Math.random()-0.5) * spaceWidth ) ;
-	    double lat = ( (Math.random()-0.5) * spaceHeight ) ;
-	    nodes.add(new Node (i, lon, lat));
-	}
+        List<Node> nodes = new ArrayList<>();
+        for(int i=0; i<nodesNb; ++i) {
+            double lon = ( (Math.random()-0.5) * spaceWidth ) ;
+            double lat = ( (Math.random()-0.5) * spaceHeight ) ;
+            nodes.add(new Node (i, lon, lat));
+        }
 
-	return nodes;
+        return nodes;
     }
 }
