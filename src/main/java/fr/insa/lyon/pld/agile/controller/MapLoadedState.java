e8dc850e28430f45415c61d9b060835f0c0125b6
Stanley Cheah <scheah@insa-lyon.fr>
2018-12-10 11:41:58 +0100

    Fix Command Pattern State: Retain state if no file is choosen while loading Map

--- src/main/java/fr/insa/lyon/pld/agile/controller/MapLoadedState.java
+++ src/main/java/fr/insa/lyon/pld/agile/controller/MapLoadedState.java
@@ -24,8 +24,8 @@ public class MapLoadedState extends InitialState {
             map.clearDeliveries();
             map.clearWarehouse();
             XMLParser.loadDeliveries(map, selectedFile.toPath());
+            controller.setCurrentState(controller.DELIVERIES_LOADED_STATE);
         }
-        controller.setCurrentState(controller.DELIVERIES_LOADED_STATE);
         cmdList.reset();
     }
     
